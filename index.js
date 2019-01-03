13b0a57b5949c8d7a109dd00ba0d73504d9288c2
Michelle Erickson <mericks@users.noreply.github.com>
2019-01-03 15:59:26 -0800

    Wireup google OAuth backend w/passport, cookie-session; mod User model

--- index.js
+++ index.js
@@ -2,14 +2,35 @@ require('dotenv').config();
 require('./lib/dbConnection');
 const express = require('express');
 const morgan = require('morgan')('dev');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const cors = require('cors')();
+
+require('./lib/services/passport');
+const errorHandler = require('./lib/services/error-handler')();
+const auth = require('./lib/routes/authRoutes');
 
 const app = express();
 app.use(express.json());
+
+app.use(
+    cookieSession({
+        // maxAage has to be passedin in milliseconds = 30 days
+        // 30 days * 24 hrs * 60 min * 60 sec * 1000 milliseconds
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        // key used to encrypt cookie
+        // array allows for multiple keys to be randomly selected for increased protection
+        keys: [process.env.COOKIE_SESSION_KEY]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(morgan);
 app.use(cors);
 
 // Routes
+app.use('/api/auth', auth);
 
 // Use static client build when in production
 if (process.env.NODE_ENV === 'production') {
@@ -19,6 +40,8 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
