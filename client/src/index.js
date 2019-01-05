c82cd2a0d2f18a83141c8178b5bbb2aab9104df9
Michelle Erickson <mericks@users.noreply.github.com>
2019-01-04 17:21:48 -0800

    Housekeeping: add notes, remove console logs, etc.

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+// Redux-Thunk allows us to manually dispatch an action at any point in time that we wish, rather than simply/immediately return it from the action creator.
 import thunk from 'redux-thunk';
 
 import App from './components/App';
