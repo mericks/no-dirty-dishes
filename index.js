a5754ceecf1897957aeda468a3823856cc096293
Michelle Erickson <mericks@users.noreply.github.com>
2019-01-25 12:14:19 -0800

    ugh, fix stupid typo

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use('/api/auth', auth);
 
 // Use static client build when in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.state('client/build'));
+    app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
